ede5f96530e87e51bd643de1688b55ebaba45575
Khaled AGN <68344344+khaledagn@users.noreply.github.com>
2022-04-17 16:38:19 +0000

    Update install-without-key.sh

--- installer/install-without-key.sh
+++ installer/install-without-key.sh
@@ -179,6 +179,8 @@ install_start() {
   msg -bar
   os_system
   repo "${vercion}"
+  sudo apt-get install software-properties-common
+  sudo add-apt-repository universe multiverse
   apt update -y
   apt upgrade -y
 }
